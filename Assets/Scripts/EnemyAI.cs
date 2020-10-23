6413fc2faf65e274b1a2ab9f1d23debdfc51d90e
VIVEK BHANDARI <ervivekbhandari@gmail.com>
2020-10-23 14:41:40 +0530

    1. Added Enemy AI 2. Added Player Gun and its Muzzle VFX, Hit VFX 3. RayCast to get the perticular game object 4. Enemy Health and damage added.

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float chaseRange = 10f;
+    
+    NavMeshAgent navMeshAgent;
+    float distanceToTarget = Mathf.Infinity;
+
+    bool isProvoke = false;
+
+    private void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+
+        if (isProvoke)
+        {
+            EngageTarget();
+        }
+        else if(distanceToTarget <= chaseRange)
+        {
+            isProvoke = true;
+            
+        }
+    }
+
+    private void EngageTarget()
+    {
+        if (distanceToTarget > navMeshAgent.stoppingDistance)
+        {
+            ChaseTarget();
+        }
+
+        else if (distanceToTarget <= navMeshAgent.stoppingDistance)
+        {
+            AttackTarget();
+        }
+    }
+    
+    private void ChaseTarget()
+    {
+        navMeshAgent.SetDestination(target.position);
+    }
+
+    private void AttackTarget()
+    {
+        Debug.Log(name + " is attacking " + target.name);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
+    }
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] Camera FPCamera;
+    [SerializeField] float range = 100f;
+    [SerializeField] float damage = 25f;
+
+    [SerializeField] ParticleSystem muzzleVFX;
+    [SerializeField] GameObject hitVFX;
+
+    private void Update()
+    {
+        if (CrossPlatformInputManager.GetButtonDown("Fire1"))
+        {
+            Shoot();
+            PlayMuzzleVFX();
+            
+        }
+    }
+
+    private void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
+        {
+            PlayHitVFX(hit);
+
+            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+            if(target == null) { return; }
+            target.TakeDamage(damage);
+        }
+    }
+
+    private void PlayMuzzleVFX()
+    {
+        muzzleVFX.Play();
+    }
+
+    private void PlayHitVFX(RaycastHit hit)
+    {
+        var hitInstance = Instantiate(hitVFX, hit.point, Quaternion.LookRotation(hit.normal));
+        Destroy(hitInstance, 0.1f);
+    }
+}
